2994744c6dc97de59684a7ac85ec3b7df223e087
Raghul T S <151618037+raghul-ts@users.noreply.github.com>
2025-07-01 23:21:52 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -43,6 +43,8 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+      ENV.rootURL = '/';
+
   }
 
   return ENV;
